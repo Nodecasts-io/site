85a2bb1dcb31235172df07a4d2f1dc9095d8dc98
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-27 12:02:51 -0700

    added public assets directory

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 'use strict';
 
+const Path = require('path');
 const Hapi = require('hapi');
+const Inert = require('inert');
 
-const server = new Hapi.Server();
-server.connection({ port: 3000 });
-
-server.register(require('inert'), (err) => {
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-      reply('Hello, world!');
-    }
-  });
-
-  server.start((err) => {
-
-      if (err) {
-          throw err;
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
       }
-      console.log('Server running at:', server.info.uri);
-  });
+    }
+  }
 });
 
+server.connection({ port: 3000 });
+
+server.register(require('inert'), () => {});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply('Hello, world!');
+  }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
