be2eecb3e4d3b9fb1d5a097477f73f3f1c7fae2e
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-27 11:57:01 -0700

    added inert

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    reply('Hello, world!');
-  }
-});
+server.register(require('inert'), (err) => {
 
-server.start((err) => {
-
-    if (err) {
-        throw err;
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      reply('Hello, world!');
     }
-    console.log('Server running at:', server.info.uri);
+  });
+
+  server.start((err) => {
+
+      if (err) {
+          throw err;
+      }
+      console.log('Server running at:', server.info.uri);
+  });
 });
+
