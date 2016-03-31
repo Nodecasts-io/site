b2d3a68aac9d56516f647df4728bcdf73d962783
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-30 19:30:59 -0700

    basic list of series

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+ express = require('express')
 const app = express()
 
 app.use(express.static('public'))
@@ -6,10 +6,14 @@ app.set('view engine', 'jade')
 
 app.get('/', (req, res) => {
   const series = require('./data/series.js')
-  console.log(series)
-  res.render('index', {title: 'Nodecasts', message: 'Welcome to Nodecasts'})
-});
+
+  res.render('index', {
+    title: 'Nodecasts',
+    message: 'Welcome to Nodecasts',
+    series: series
+  })
+})
 
 app.listen(3000, () => {
   console.log('Example app listening at http://localhost:3000')
-});
+})
