70f475379cd2ce0f269cfcd2405ee9fee5b10071
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-30 18:47:07 -0700

    add series and jade

---
+++ data/series.js
@@ -0,0 +1,110 @@
+module.exports = [
+  {
+    name: "Mastering Chrome DevTools",
+    videos: [
+      {
+        title: "Advanced Chrome DevTools",
+        url: "https://www.youtube.com/watch?v=0eSAsi3GPh4"
+      },
+      {
+        title: "Chrome Devtools - Black Boxing Files",
+        url: "https://www.youtube.com/watch?v=HuQdWw8YOMo"
+      },
+      {
+        title: "Chrome Devtools - Workspaces",
+        url: "https://www.youtube.com/watch?v=thGaLbbKsW0"
+      }
+    ]
+  },
+  {
+    name: "JavaScript ES6",
+    videos: [
+      {
+        title: "JavaScript ES6 - Introduction",
+        url: "https://www.youtube.com/watch?v=HIS8juawTmM"
+      },
+      {
+        title: "JavaScript ES6 - Running ES6 on the front end and back end",
+        url: "https://www.youtube.com/watch?v=F0n4pN4ckdI"
+      },
+      {
+        title: "JavaScript ES6 - Destructuring",
+        url: "https://www.youtube.com/watch?v=eAPtATp5DLg"
+      },
+      {
+        title: "JavaScript ES6 - var, let and const",
+        url: "https://www.youtube.com/watch?v=1sHq04erG9o"
+      },
+      {
+        title: "JavaScript ES6 - Spread Operator",
+        url: "https://www.youtube.com/watch?v=8U3ZTCtZy5Q"
+      },
+      {
+        title: "JavaScript ES6 - Arrow Functions",
+        url: "https://www.youtube.com/watch?v=1UDfBRd51EI"
+      },
+      {
+        title: "JavaScript ES6 - Template Literals",
+        url: "https://www.youtube.com/watch?v=ZyFOrB6VyTQ"
+      },
+      {
+        title: "JavaScript ES6 - Classes",
+        url: "https://www.youtube.com/watch?v=MsbNJPsjD-w"
+      },
+      {
+        title: "JavaScript ES6 - Symbols",
+        url: "https://www.youtube.com/watch?v=ku_s_Aw_ms4"
+      }
+    ]
+  },
+  {
+    name: "Immutable.js",
+    videos: [
+      {
+        title: "Introduction to Immutable.js",
+        url: "https://www.youtube.com/watch?v=ewMKXHu1E1Q"
+      },
+      {
+        title: "Immutable.js part 1 - Map",
+        url: "https://www.youtube.com/watch?v=2BZ6LY9LCsg"
+      },
+      {
+        title: "Immutable.js part 2 - List",
+        url: "https://www.youtube.com/watch?v=Ihxddhq8XeE"
+      }
+    ]
+  },
+  {
+    name: "Don't worry, be Hapi",
+    videos: [
+      {
+        title: "Introduction to Hapi.js",
+        url: "https://www.youtube.com/watch?v=V-txbAYtVVE"
+      },
+      {
+        title: "Hapi.js Part 2 - Routing",
+        url: "https://www.youtube.com/watch?v=zVavqDMmttw"
+      },
+      {
+        title: "Hapi.js Part 3 - Static Content",
+        url: "https://www.youtube.com/watch?v=uPKlktGFiyw"
+      },
+      {
+        title: "Hapi.js Part 4 - Introduction to views",
+        url: "https://www.youtube.com/watch?v=PVFUzNRTid4"
+      },
+      {
+        title: "Hapi.js Part 5 - Connecting to a database",
+        url: "https://www.youtube.com/watch?v=sfvG4pFeuQw"
+      },
+      {
+        title: "Hapi.js Part 6 - Integrating Socket.io",
+        url: "https://www.youtube.com/watch?v=HDQAsh0Bc1E"
+      },
+      {
+        title: "Hapi.js Part 7 - Socket.io view counter with Hapi.js",
+        url: "https://www.youtube.com/watch?v=KX4fnjBJQAM"
+      }
+    ]
+  }
+]
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
+app.set('view engine', 'jade')
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'index.html')
+  const series = require('./data/series.js')
+  console.log(series)
+  res.render('index', {title: 'Nodecasts', message: 'Welcome to Nodecasts'})
 });
 
 app.listen(3000, () => {
