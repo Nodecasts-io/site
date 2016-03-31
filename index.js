70da98f0e2311802524200bd2e5dc17af797e3e7
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-30 20:54:10 -0700

    separated out routes

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express')
-const series  = require('./data/series.js')
 const app     = express()
+
+const routes = require('./routes/index')
 
 app.use(express.static('public'))
 app.set('view engine', 'jade')
-
-app.get('/', (req, res) => {
-  const entries = series.map((entry) => {
-    return {
-      title: entry.name,
-      count: entry.videos.length
-    }
-  })
-
-  res.render('index', {
-    title: 'Nodecasts',
-    message: 'Welcome to Nodecasts',
-    entries: entries
-  })
-})
+app.use('/', routes)
 
 app.listen(3000, () => {
   console.log('Example app listening at http://localhost:3000')
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const series  = require(__dirname + '/../data/series.js')
+const router  = express.Router()
+
+router.get('/', (req, res, next) => {
+  const entries = series.map((entry) => {
+    return {
+      title: entry.name,
+      count: entry.videos.length
+    }
+  })
+
+  res.render('index', {
+    title: 'Nodecasts',
+    message: 'Welcome to Nodecasts',
+    entries: entries
+  })
+})
+
+module.exports = router
