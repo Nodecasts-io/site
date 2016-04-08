175681324d5a6330dd5eca12c0307a0236b94046
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-07 20:26:05 -0700

    rename config file

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const routes  = require('./routes/index')
 const series  = require('./routes/series')
+const google  = require('googleapis')
+const youtube = google.youtube('v3')
 const app     = express()
+
+const params = {
+  auth: 'AIzaSyBoSKnl0Bmp2Q8K5QhYBFs67Qd5AvGvenw',
+  part: 'contentDetails',
+  maxResults: 50,
+  playlistId: 'PLVHlCYNvnqYouIVj3IgK3RmzpnWMaoqkw'
+}
+
+youtube.playlistItems.list(params, function(err, playlist) {
+  console.log(playlist)
+})
 
 // Middleware
 app.use(express.static('public'))
