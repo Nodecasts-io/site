4ddb73d104c1253650130925fcc090a7b3dba6aa
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-21 00:15:02 -0700

    added compression for gzip

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express  = require('express')
-const routes   = require('./routes/index')
-const video    = require('./routes/video')
-const playlist = require('./routes/playlist')
-const app      = express()
+const express     = require('express')
+const compression = require('compression')
+const routes      = require('./routes/index')
+const video       = require('./routes/video')
+const playlist    = require('./routes/playlist')
+const app         = express()
 
 // Middleware
+app.use(compression())
 app.use(express.static('public'))
 app.set('view engine', 'jade')
 
