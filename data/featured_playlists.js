171371cc6d69d9062906d5f3b3c0a260963c66a5
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-22 15:54:24 -0700

    back to old style exports

--- data/featured_playlists.js
+++ data/featured_playlists.js
@@ -1,4 +1,4 @@
-export default [
+module.exports = [
   'PLVHlCYNvnqYouIVj3IgK3RmzpnWMaoqkw',
   'PLVHlCYNvnqYpQXeTEA0PxH1spth-K9ey7',
   'PLVHlCYNvnqYrUeBZP0pUxSFT0KYr9Xw84',
--- data/featured_videos.js
+++ data/featured_videos.js
@@ -1,4 +1,4 @@
-export default [
+module.exports = [
   'V-txbAYtVVE',
   '8UsIexAY4NY',
   '0eSAsi3GPh4',
