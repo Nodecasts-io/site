cf59cbe4d3337a88b70a9a9d9d8e5468100e5a21
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-21 23:41:54 -0700

    videos and courses working!

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express     = require('express')
 const compression = require('compression')
 const routes      = require('./routes/index')
 const video       = require('./routes/video')
+const videos       = require('./routes/videos')
 const playlist    = require('./routes/playlist')
+const playlists    = require('./routes/playlists')
 const app         = express()
 
 // Middleware
@@ -13,7 +15,9 @@ app.set('view engine', 'jade')
 // Routes
 app.use('/', routes)
 app.use('/video', video)
+app.use('/videos', videos)
 app.use('/playlist', playlist)
+app.use('/courses', playlists)
 
 app.listen(3000, () => {
   console.log('Example app listening at http://localhost:3000')
---
+++ routes/videos.js
@@ -0,0 +1,31 @@
+const express        = require('express')
+const google         = require('googleapis')
+const router         = express.Router()
+const youtube        = google.youtube('v3')
+const defaultConfig  = require('../config')
+
+router.get('/', (req, res) => {
+  const vids = []
+
+  const config = Object.assign({
+    part: 'snippet',
+    maxResults: 50,
+    type: 'video'
+  }, defaultConfig)
+
+  youtube.search.list(config, (err, videos) => {
+    videos.items.map((video) => {
+      vids.push({
+        title: video.snippet.title,
+        id: video.id
+      })
+    })
+
+    res.render('videos', {
+      title: 'Nodecasts',
+      videos: vids
+    })
+  })
+})
+
+module.exports = router
