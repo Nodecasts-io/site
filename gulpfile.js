8064b02f011e15c956bd51da0db83bcd0a021118
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-22 18:57:07 -0700

    deleted unecessary css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
-const gulp = require('gulp')
+const gulp     = require('gulp')
+const uglify   = require('gulp-uglify')
+const concat   = require('gulp-concat')
+const cleanCSS = require('gulp-clean-css')
 
-gulp.task('default', () => {
-  // place code for your default task here
+gulp.task('default', ['minify-js', 'minify-css'], () => {})
+
+gulp.task('minify-js', () => {
+  return gulp.src('public/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('public/bundle'))
+})
+
+gulp.task('minify-css', () => {
+  return gulp.src('public/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('public/bundle'))
 })
