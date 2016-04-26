87e525a436e377f1be315e47977f1782b8f101b7
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-26 10:30:50 -0700

    added node playlist to index

--- data/featured_playlists.js
+++ data/featured_playlists.js
@@ -1,6 +1,6 @@
 module.exports = [
+  'PLVHlCYNvnqYqjnypg2Czw4vVjTL2gB7_e',
+  'PLVHlCYNvnqYrUeBZP0pUxSFT0KYr9Xw84',
   'PLVHlCYNvnqYouIVj3IgK3RmzpnWMaoqkw',
   'PLVHlCYNvnqYpQXeTEA0PxH1spth-K9ey7',
-  'PLVHlCYNvnqYrUeBZP0pUxSFT0KYr9Xw84',
-  'PLVHlCYNvnqYpSUXgYY1QwBx96QUH-3-l4'
 ]
